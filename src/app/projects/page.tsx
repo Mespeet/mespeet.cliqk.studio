b98993f6a247a0076d0015d4e3522f9fe7d76a9c
Mespeet <mespeet@gmail.com>
2024-10-08 11:13:49 +0200

    Another commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import {
   Card,
   CardHeader,
-  CardBody,
-  CardFooter,
-  Button,
-  Chip,
-  Tooltip
 } from "@nextui-org/react";
 
 export default function Projects() {
